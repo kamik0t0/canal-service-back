954f269abd666f2b251625aacd8f4c6fa3e7b4d3
kamik0t0 <Cap_NEMOx86@inbox.ru>
2022-05-06 20:01:05 +0500

    initial

--- app.js
+++ app.js
@@ -12,5 +12,19 @@ app.use(express.static(path.join(__dirname, "public")));
 // app.use("/", indexRouter);
 app.use("/", getDataController);
 
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+});
+
 // app.listen(PORT, () => console.log(`SERVER STARTS ON PORT ${PORT}`));
 module.exports = app;
