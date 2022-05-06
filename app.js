a2eab8e6d8deaf155c819d77977e4f66a7ce5111
kamik0t0 <Cap_NEMOx86@inbox.ru>
2022-05-06 20:58:15 +0500

    final

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const dataController = require("./controllers/get-data-controller.js");
 const app = express();
 app.use(setHeaders);
 app.use("/", dataController);
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 
 // app.use("/", indexRouter);
 
