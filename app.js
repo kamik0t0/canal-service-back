98a0e4c5623f890a4484f931751f4681e6a1ce6c
kamik0t0 <Cap_NEMOx86@inbox.ru>
2022-05-06 19:47:10 +0500

    initial

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const path = require("path");
+const setHeaders = require("./middleware/headers.js");
 // const PORT = process.env.PORT || 5700;
 const indexRouter = require("./routes/index");
 
 const app = express();
-
+app.use(setHeaders);
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
