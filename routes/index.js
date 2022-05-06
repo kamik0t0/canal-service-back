38b0b2ceee197d8bf8f2079da5a001340bdf08e2
kamik0t0 <Cap_NEMOx86@inbox.ru>
2022-05-06 19:49:42 +0500

    initial

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const getDataController = require("../controllers/get-data-controller.js");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get("/", getDataController);
 
 module.exports = router;
