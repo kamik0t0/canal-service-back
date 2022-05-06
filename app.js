6adc95f17858be32d2d690e8588e6b4bc4889131
kamik0t0 <Cap_NEMOx86@inbox.ru>
2022-05-06 21:01:50 +0500

    final

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const path = require("path");
 const setHeaders = require("./middleware/headers.js");
-// const PORT = process.env.PORT || 5700;
+const PORT = process.env.PORT || 5700;
 const indexRouter = require("./routes/index.js");
 const dataController = require("./controllers/get-data-controller.js");
 
 const app = express();
 app.use(setHeaders);
-app.use("/", dataController);
+app.get("/", dataController);
 // app.use(express.static(path.join(__dirname, "public")));
 
 // app.use("/", indexRouter);
 
-// app.listen(PORT, () => console.log(`SERVER STARTS ON PORT ${PORT}`));
+app.listen(PORT, () => console.log(`SERVER STARTS ON PORT ${PORT}`));
 module.exports = app;
